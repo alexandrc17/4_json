541593ded0594997f905fde9c21788cb14723c6b
alexandrc17 <55381345+alexandrc17@users.noreply.github.com>
2019-09-18 13:39:28 +0300

    Code

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,11 @@
 import json
-
-
-def load_data(filepath):
-    pass
+with open(r"C:\Users\860159\Documents\GitHub\4_json\als.json", "r", encoding='UTF-8') as filepath:
+    data = json.load(filepath)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+     pass
